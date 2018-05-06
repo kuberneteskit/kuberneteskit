a47e5a5b964a975c0b8e6b80aaf0d0f0a547f273
Jason DeTiberus <detiber@gmail.com>
2018-05-06 12:48:55 -0400

    Fix kubelet package
    - Build kubernetes packages from source. This is required because the
      kubelet binary from the kubernetes-node tarball is not a static binary
    - Create /etc/kubernetes-release file with the k8s version
    - Fix missing runkubelet.sh and runkubeadm.sh binaries
    - Create a marker file (/etc/kubernetes/.kubeadm.init.finished) to
      indicate that kubeadm init has finished running.

--- pkg/kubelet/runkubeadm.sh
+++ pkg/kubelet/runkubeadm.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 if [[ -f /run/config/kubernetes/kubeadm-init ]]; then
-  kubeadm init -- config /run/config/kubernetes/kubeadm-init --ignore-preflight-errors=FileExisting-iptables
+  kubeadm init --config /run/config/kubernetes/kubeadm-init --ignore-preflight-errors=FileExisting-iptables
   export KUBECONFIG=/etc/kubernetes/admin.conf
   kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubeadm/1.7/calico.yaml
+  touch /etc/kubernetes/.kubeadm.init.finished
 fi
