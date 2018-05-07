5f112bf1a7addfaadff842ec861ccbf25470ec71
Jason DeTiberus <detiber@gmail.com>
2018-05-06 22:58:20 -0400

    Fix issue with boot_cluster.sh script

--- boot_cluster.sh
+++ boot_cluster.sh
@@ -17,5 +17,5 @@ for node_num in $(seq 1 ${KUBE_NODE_COUNT}); do
   echo "Booting node instance ${node_num}..."
   node_name=kube-node-${node_num}
   mkdir -p ${node_name}-state
-  KUBE_NODE_TYPE=node KUBE_NODE_NAME=${node_name} ./boot.sh &> ${node_name}-state/console.out &
+  KUBE_HOST_TYPE=node KUBE_HOST_NAME=${node_name} ./boot.sh &> ${node_name}-state/console.out &
 done
